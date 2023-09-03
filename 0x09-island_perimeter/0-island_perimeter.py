ce73822d97e19a2edbea38cfd89b9ce2f11c9cb3
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-03 23:02:45 +0100

    funct

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
-"""
-a function def island_perimeter(grid): that returns the perimeter of the
-island described in grid.
-"""
-from typing import List
+''' island perimeter module '''
 
 
-def island_perimeter(grid: List[List]) -> int:
-    """
-    return the perimeter of the island, which is the length
-    and breath surrounded by water.
+def island_perimeter(grid):
+    ''' calculates perimeter of an island '''
 
-    Args:
-        grid (List[List]): List of list of zeroes and ones.
-        where 0's signifies water and 1's signify island.
+    row_count = len(grid)
+    col_count = len(grid[0])
 
-    Returns:
-        int: the perimeter.
-    """
-    length = 0
-    breadth = 0
+    p = 0
+    for i in range(row_count):
+        for j in range(col_count):
+            if grid[i][j]:
+                u = (not i) or grid[i - 1][j] == 0
+                d = (i + 1 >= row_count) or grid[i + 1][j] == 0
+                l = (not j) or grid[i][j - 1] == 0
+                r = (j + 1 >= col_count) or grid[i][j + 1] == 0
+                p += u + d + l + r
+    return p
+# """
+# a function def island_perimeter(grid): that returns the perimeter of the
+# island described in grid.
+# """
 
-    for row in grid:
-        if any(row):
-            length += 1
-            # if the number of 1 is not greater than one then
-            # we won't add to the breadth.
-            b = row.count(1)
-            breadth += b if b != 1 else 0
 
-    return 2 * (length + breadth)
+# def island_perimeter(grid: List[List]) -> int:
+#     """
+#     return the perimeter of the island, which is the length
+#     and breath surrounded by water.
+
+#     Args:
+#         grid (List[List]): List of list of zeroes and ones.
+#         where 0's signifies water and 1's signify island.
+
+#     Returns:
+#         int: the perimeter.
+#     """
+#     length = 0
+#     breadth = 0
+
+#     for row in grid:
+#         if any(row):
+#             length += 1
+#             # if the number of 1 is not greater than one then
+#             # we won't add to the breadth.
+#             b = row.count(1)
+#             if breadth < 1:
+#                 breadth += b if b != 1 else 0
+                
+#     # if length is 1 at least then breadth must be 1
+#     if length and not breadth:
+#         breadth = 1
+#     return 2 * (length + breadth)
